7e032c073871aa76f12c26b4c2d1d09347f85082
jonasstibolt <jonasholmgaard@gmail.com>
2024-12-29 11:40:42 +0100

    latest commit

--- wodder/mainapp/models.py
+++ wodder/mainapp/models.py
@@ -3,14 +3,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    name = models.CharField(max_length=18)
+    name = models.CharField(max_length=18, null=False)
     bio = models.TextField(max_length=300)
     profile_pic = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
 
 
     def __str__(self):
         return self.name
-
-
-
-
